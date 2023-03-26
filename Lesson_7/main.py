98d5b1a0321cba0db884479f877a5b1762b67688
Vlad <linnikvitaly@gmail.com>
2023-03-26 16:50:23 +0300

    Сделан калькулятор с проверкой

---
+++ Lesson_7/main.py
@@ -0,0 +1,12 @@
+def input_int(message):
+    while True:
+        try:
+            return int(input(message))
+        except ValueError:
+            print(f'Неправельное число. Попробуйте снова')
+
+
+a = input_int('Введите A:')
+b = input_int('Введите B:')
+
+print(a + b)
