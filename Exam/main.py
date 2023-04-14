0f97ccef6571ac5082e0ce2102a9d743bfdb9f5d
Vlad <linnikvitaly@gmail.com>
2023-04-14 11:10:50 +0300

    Экзамен

--- Exam/main.py
+++ Exam/main.py
@@ -1,5 +1,6 @@
 from person import Person, Action, Rest, Work
 import random
+from colorama import Fore
 
 e1 = ''
 e2 = ''
@@ -10,35 +11,39 @@ human_2 = Person(name="Ваня", health=100, mood=100, money=10)
 human_3 = Person(name="Влад", health=100, mood=100, money=10)
 people = [human_1, human_2, human_3]
 
-go_to_hospital = Action(name='Сходить в больницу', health=20, mood=-10, money=-7)
-go_to_park = Rest(name='Сходить в парк', health=5, mood=20, money=-5)
-work_on_factory = Work(name='Поработать на заводе', health=-9, mood=-10, money=20)
+go_to_hospital = Action(name='Сходить в больницу', health=20, mood=-5, money=-20)
+go_to_park = Rest(name='Сходить в парк', health=3, mood=15, money=0)
+work_on_factory = Work(name='Поработать на заводе', health=-3, mood=-10, money=50)
 actions = [go_to_park, go_to_hospital, work_on_factory]
+
+print('-----Real Life Simulator-----', Fore.MAGENTA)
 
 while len(people) > 0:
     if human_1 in people:
-        print(f'\n{human_1}')
+        print(f'\n{human_1}', Fore.BLUE)
         try:
             human_1.do(random.choice(actions))
         except ValueError as e:
             print(f'\n{human_1.name} {e}')
-            e1 = f'{human_1.name} {e}'
+            e1 = f'{human_1.name} {e} {Fore.RED}'
+            print(e1)
             people.remove(human_1)
     if human_2 in people:
-        print(f'\n{human_2}')
+        print(f'\n{human_2}', Fore.GREEN)
         try:
             human_2.do(random.choice(actions))
         except ValueError as e:
             print(f'\n{human_2.name} {e}')
-            e2 = f'{human_2.name} {e}'
+            e2 = f'{human_2.name} {e} {Fore.RED}'
+            print(e2)
             people.remove(human_2)
     if human_3 in people:
-        print(f'\n {human_3}')
+        print(f'\n {human_3}', Fore.YELLOW)
         try:
             human_3.do(random.choice(actions))
         except ValueError as e:
-            print(f'\n{human_3.name} {e}')
             e3 = f'{human_3.name} {e}'
+            print(e3)
             people.remove(human_3)
 
-print(f'\n\n{e1} \n{e2} \n{e3} \n\nВсе люди умерли впали в дипрессию или абонкротились :(')
+print(f'\n\n{e1} \n{e2} \n{e3} \n\nВсе люди умерли впали в дипрессию или абонкротились :( {Fore.RED}')
--- Exam/person.py
+++ Exam/person.py
@@ -1,3 +1,4 @@
+from colorama import Style, Fore
 class Person:
     name = ''
     health = 0
@@ -29,17 +30,18 @@ class Person:
         self.mood += action.mood
         self.money += action.money
 
-
     def do(self, action):
-        if type(Action):
-            self.health += action.health
-            self.mood += action.mood
-            self.money += action.money
+        if isinstance(action, Action):
+            if isinstance(action, Work):
+                if self.mood > 90:
+                    action.money += action.money * 0.1.__round__(0)
+            elif isinstance(action, Rest):
+                if self.health < 40:
+                    action.mood += action.mood * 0.8.__round__(0)
+            self.change_state(health=action.health, mood=action.mood, money=action.money, action=action)
 
-            if type(Work) and self.mood > 90:
-                self.money += int(action.money * 0.1)
-            elif type(Rest) and self.health < 40:
-                self.mood += int(action.mood * 0.2)
+
+
 
 class Action:
     def __init__(self, name, health=0, mood=0, money=0):
