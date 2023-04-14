79ee9c5d5a6a55d973258b14e08b9938182bce0b
Vlad <linnikvitaly@gmail.com>
2023-04-14 09:27:33 +0300

    1

--- Exam/main.py
+++ Exam/main.py
@@ -1,7 +1,44 @@
-from person import Person
-human_1 = Person(name="Васьок", health=100, mood=100, money=10)
-print(human_1)
-print('')
+from person import Person, Action, Rest, Work
+import random
 
-human_1.change_state(health=-10, mood=-10, money=15)
-print(human_1)
+e1 = ''
+e2 = ''
+e3 = ''
+
+human_1 = Person(name="Вася", health=100, mood=100, money=10)
+human_2 = Person(name="Ваня", health=100, mood=100, money=10)
+human_3 = Person(name="Влад", health=100, mood=100, money=10)
+people = [human_1, human_2, human_3]
+
+go_to_hospital = Action(name='Сходить в больницу', health=20, mood=-10, money=-7)
+go_to_park = Rest(name='Сходить в парк', health=5, mood=20, money=-5)
+work_on_factory = Work(name='Поработать на заводе', health=-9, mood=-10, money=20)
+actions = [go_to_park, go_to_hospital, work_on_factory]
+
+while len(people) > 0:
+    if human_1 in people:
+        print(f'\n{human_1}')
+        try:
+            human_1.do(random.choice(actions))
+        except ValueError as e:
+            print(f'\n{human_1.name} {e}')
+            e1 = f'{human_1.name} {e}'
+            people.remove(human_1)
+    if human_2 in people:
+        print(f'\n{human_2}')
+        try:
+            human_2.do(random.choice(actions))
+        except ValueError as e:
+            print(f'\n{human_2.name} {e}')
+            e2 = f'{human_2.name} {e}'
+            people.remove(human_2)
+    if human_3 in people:
+        print(f'\n {human_3}')
+        try:
+            human_3.do(random.choice(actions))
+        except ValueError as e:
+            print(f'\n{human_3.name} {e}')
+            e3 = f'{human_3.name} {e}'
+            people.remove(human_3)
+
+print(f'\n\n{e1} \n{e2} \n{e3} \n\nВсе люди умерли впали в дипрессию или абонкротились :(')
--- Exam/person.py
+++ Exam/person.py
@@ -17,14 +17,42 @@ class Person:
             f'Настроение: {self.mood}\n' \
             f'Деньги: {self.money}'
 
-    def change_state(self, health, mood, money):
-        if self.health < 0:
-            raise SyntaxError('Человек умер')
-        if self.mood < 0:
-            raise ValueError('Человек впал в депрессию')
-        if self.money < 0:
-            raise TypeError('Человек обанкротился')
+    def change_state(self, action, health=0, mood=0, money=0):
+        if self.health + health < 0:
+            raise ValueError('умер')
+        if self.mood + mood < 0:
+            raise ValueError('впал в дипрессию')
+        if self.money + money < 0:
+            raise ValueError('абонкротился')
 
-        self.health += health
-        self.mood += mood
-        self.money += money
+        self.health += action.health
+        self.mood += action.mood
+        self.money += action.money
+
+
+    def do(self, action):
+        if type(Action):
+            self.health += action.health
+            self.mood += action.mood
+            self.money += action.money
+
+            if type(Work) and self.mood > 90:
+                self.money += int(action.money * 0.1)
+            elif type(Rest) and self.health < 40:
+                self.mood += int(action.mood * 0.2)
+
+class Action:
+    def __init__(self, name, health=0, mood=0, money=0):
+        self.name = name
+        self.health = health
+        self.mood = mood
+        self.money = money
+
+class Rest(Action):
+    def __init__(self, name, health=0, mood=0, money=0):
+        Action.__init__(self, name, health, mood, money)
+
+
+class Work(Action):
+    def __init__(self, name, health=0, mood=0, money=0):
+        Action.__init__(self, name, health, mood, money)
